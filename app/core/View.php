1d44d9c26e43d0f1db1d8b74fa5f8a354380d871
natedrake <john1o1grady@hotmail.com>
2016-04-03 22:35:31 +0100

    update View.php to include header and footer inc files

--- app/core/View.php
+++ app/core/View.php
@@ -2,30 +2,77 @@
 
 namespace Ecne\Core;
 
-use Ecne\Templator\Templator;
+use Ecne\Form\Form;
+use \Ecne\Library\Core\File;
+use \Ecne\Library\Core\Helper;
 
 class View
 {
     /**
-     *  @var $templator|Templator
+     * @var string
      */
-    private $templator = null;
-
+    private $view='';
     /**
-     * @param $view
+     * @var array
      */
-    function __construct($view)
+    private $data=array();
+    /**
+     * @var string
+     */
+    protected $output;
+
+
+    function __construct()
     {
-        $this->templator = new Templator((VIEW_PATH . $view . '.htm'));
+
     }
 
     /**
-     *  @method render
-     *  @param $data|array
-     *  @return void
+     * @return mixed
      */
-     public function render($data = array())
+    public function loginForm()
+    {
+        return $this->loginForm;
+    }
+
+    /**
+     * @method form
+     * @access public
+     * @return \Ecne\Form\Form
+     */
+    public function form()
+    {
+        return $this->form;
+    }
+
+    public function data($name)
+    {
+        if (isset($this->data[$name])) {
+            return $this->data[$name];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param $view
+     * @param array $data
+     * @throws \Exception
+     */
+     public function render($view = null, $data = array())
      {
-         $this->templator->render($data);
+         $this->view = $view;
+         $this->data = $data;
+         if (File::exists(VIEW_PATH.$this->view.'.php')) {
+             ob_start();
+             include_once VIEW_PATH.'layout.php';
+             include_once VIEW_PATH . $this->view.'.php';
+             include_once VIEW_PATH.'footer.php';
+             $this->output = ob_get_contents();
+             ob_end_clean();
+             echo $this->output;
+         } else {
+             throw new \Exception('View {$view} does not exist');
+         }
      }
 }
